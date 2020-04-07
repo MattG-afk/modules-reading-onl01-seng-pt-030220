9194011354403614b04611a3233a84fcae3ddf98
Matt Gordon <bigmattsize21@gmail.com>
2020-04-07 21:02:14 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,6 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  include Dance 
   
   attr_accessor :name 
   
@@ -10,7 +9,7 @@ class Kid
     @name = name
   end
   
-  extend MetaDancing
+  
   
   
 end
